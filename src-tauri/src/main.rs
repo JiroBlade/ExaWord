bd6191d5c1cee6bb9f3c7f0bf03e9ea42e792c18
Sandun Wiratunga <79461263+sandunwira@users.noreply.github.com>
2023-04-22 05:44:01 +0530

    Did some renovating to the app

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,9 +3,14 @@
   windows_subsystem = "windows"
 )]
 
+#[derive(Clone, serde::Serialize)]
+struct Payload {
+  args: Vec<String>,
+  cwd: String,
+}
+
 use std::{fs::File, io::Write};
 use tauri::{api::dialog::blocking::FileDialogBuilder, Manager};
-use windows::{core::*, Data::Xml::Dom::*, Foundation::*, Storage::*};
 use window_shadows::set_shadow;
 
 fn main() {
@@ -15,6 +20,11 @@ fn main() {
       set_shadow(&main_window, true).unwrap();
       Ok(())
     })
+    .plugin(tauri_plugin_single_instance::init(|app, argv, cwd| {
+      println!("{}, {argv:?}, {cwd}", app.package_info().name);
+      app.emit_all("single-instance", Payload { args: argv, cwd }).unwrap();
+    })) // For Blocking Multiple Instances
+    .plugin(tauri_plugin_window_state::Builder::default().build()) // For Saving Window States
     .invoke_handler(tauri::generate_handler![save_file])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
@@ -22,26 +32,26 @@ fn main() {
 
 #[tauri::command(async)]
 async fn save_file(win: tauri::Window, contents: Vec<String>) {
+  let default_filename = if let Some(first_line) = contents.first() {
+    Some(first_line.trim())
+  } else {
+    None
+  };
+
   let filepath = FileDialogBuilder::new()
     .set_parent(&win)
+    .add_filter("Text Document", &["txt"])
     .add_filter("Word Document", &["xml"])
-    .save_file().unwrap_or_default();
+    .add_filter("Markdown Document", &["md"])
+    .set_file_name(default_filename.unwrap_or_default())
+    .save_file()
+    .unwrap_or_default();
 
-  if filepath.capacity() != 0 {
-    let file = File::create(filepath).unwrap();
-
-    xml(file).unwrap();
+  if !filepath.exists() {
+    let mut file = File::create(filepath).unwrap();
+    for line in contents {
+      file.write_all(line.as_bytes()).unwrap();
+      file.write_all(b"\n").unwrap();
+    }
   }
-}
-
-fn xml<W: Write>(mut w: W) -> Result<()> {
-  let doc = XmlDocument::new()?;
-  let root = doc.CreateElement(h!("root"))?;
-  
-  
-  
-  
-  w.write_all(doc.GetXml().unwrap().to_string().as_bytes()).unwrap();
-
-  Ok(())
 }
